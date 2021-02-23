8cc29b52794928657067ab13fb5874964025e352
choijinhap <wlsgkq123@gmail.com>
2021-02-23 19:47:32 +0000

    #2 step functionality

---
+++ #2_Progress_Step/app.js
@@ -0,0 +1,41 @@
+const progress = document.getElementById('progress');
+const circles = document.querySelectorAll('.circle');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let activeCircle = 1;
+
+next.addEventListener('click', () => {
+  if (activeCircle < circles.length) {
+    activeCircle++;
+  }
+  update();
+});
+
+prev.addEventListener('click', () => {
+  if (activeCircle > 1) {
+    activeCircle--;
+  }
+  update();
+});
+
+const update = () => {
+  progress.style.width =
+    ((activeCircle - 1) / (circles.length - 1)) * 100 + '%';
+  circles.forEach((circle, idx) => {
+    if (activeCircle > idx) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  if (activeCircle >= circles.length) {
+    next.disabled = true;
+  } else if (activeCircle === 1) {
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+};
