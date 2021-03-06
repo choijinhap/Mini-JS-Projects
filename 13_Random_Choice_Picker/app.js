0b343472c6593c0f9b990510806a25fba685d9f2
choijinhap <wlsgkq123@gmail.com>
2021-03-06 20:30:23 +0000

    #13 random selecting

--- 13_Random_Choice_Picker/app.js
+++ 13_Random_Choice_Picker/app.js
@@ -5,10 +5,15 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
 	createTags(e.target.value);
+	if (e.key === 'Enter') {
+		setTimeout(() => {
+			e.target.value = '';
+		}, 10);
+		randomSelect();
+	}
 });
 
 function createTags(input) {
-	console.log(input);
 	const tags = input
 		.split(',')
 		.filter((tag) => tag.trim() !== '')
@@ -20,4 +25,37 @@ function createTags(input) {
 		tagEl.innerText = tag;
 		tagsEl.appendChild(tagEl);
 	});
+}
+
+function randomSelect() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+		highlightTag(randomTag);
+		setTimeout(() => {
+			unHighlightTag(randomTag);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+			highlightTag(randomTag);
+		}, 100);
+	}, times * 100);
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll('.tag');
+	const randomIdx = Math.floor(Math.random() * tags.length);
+	return tags[randomIdx];
+}
+
+function highlightTag(tag) {
+	tag.classList.add('highlight');
+}
+function unHighlightTag(tag) {
+	tag.classList.remove('highlight');
 }
